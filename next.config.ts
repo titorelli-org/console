f222ab1569afa8003965fbeaf54f4bfe408bead8
ychebotaev <yury.79120345101@gmail.com>
2025-04-12 14:35:43 +0500

    feat: add authorize errors support

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    SITE_ORIGIN: process.env.SITE_ORIGIN
-  }
+    SITE_ORIGIN: process.env.SITE_ORIGIN,
+  },
+  experimental: {
+    authInterrupts: true,
+  },
 };
 
 export default nextConfig;
