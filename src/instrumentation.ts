2c28bd190edb4409943059c96bb8f31acbe6b208
ychebotaev <yury.79120345101@gmail.com>
2025-04-15 23:11:28 +0500

    feat: oo traces enable

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,19 @@
-import { registerOTel } from "@vercel/otel";
+import { PrismaInstrumentation } from "@prisma/instrumentation";
+import { registerOTel, OTLPHttpJsonTraceExporter } from "@vercel/otel";
 
 export function register() {
-  registerOTel({ serviceName: "console" });
+  registerOTel({
+    serviceName: "console",
+    traceExporter: new OTLPHttpJsonTraceExporter({
+      url: `${process.env.OO_BASE_URL}/v1/traces`,
+      headers: {
+        Authorization: `Basic ${process.env.OO_AUTH_CRED}`,
+      },
+    }),
+    instrumentations: [
+      new PrismaInstrumentation({
+        middleware: true,
+      }),
+    ],
+  });
 }
