eb32fcdcb470aed69357a9228c8256caeca92bd3
ychebotaev <yury.79120345101@gmail.com>
2025-04-22 23:46:36 +0500

    chore: return as it was )

--- src/lib/server/env.ts
+++ src/lib/server/env.ts
@@ -1,23 +1,28 @@
 import "server-only";
 
-import { cleanEnv, str, url, num } from "envalid";
+import { cleanEnv, str, url, num, CleanedEnv } from "envalid";
 
-export const env =
-  process.env.NEXT_PHASE === "phase-production-build"
-    ? process.env
-    : cleanEnv(process.env, {
-        NODE_ENV: str(),
-        SITE_ORIGIN: url(),
-        API_ORIGIN: url(),
-        DATABASE_URL: str(),
-        PASSWORD_PEPPER: str(),
-        JWT_SECRET: str(),
-        SMTP_PASS_RESTORE_PASSWORD: str(),
-        SMTP_PASS_NOREPLY: str(),
-        TITORELLI_SERVICE_URL: url(),
-        TITORELLI_CLIENT_SECRET: str(),
-        OO_BASE_URL: url(),
-        OO_AUTH_CRED: str(),
-        KEYMASK_SEED: str(),
-        NEXT_PUBLIC_YM_COUNTER_ID: num(),
-      });
+const config = {
+  NODE_ENV: str(),
+  SITE_ORIGIN: url(),
+  API_ORIGIN: url(),
+  DATABASE_URL: str(),
+  PASSWORD_PEPPER: str(),
+  JWT_SECRET: str(),
+  SMTP_PASS_RESTORE_PASSWORD: str(),
+  SMTP_PASS_NOREPLY: str(),
+  TITORELLI_SERVICE_URL: url(),
+  TITORELLI_CLIENT_SECRET: str(),
+  OO_BASE_URL: url(),
+  OO_AUTH_CRED: str(),
+  KEYMASK_SEED: str(),
+  NEXT_PUBLIC_YM_COUNTER_ID: num(),
+} as const;
+
+export let env: CleanedEnv<typeof config>;
+
+if (process.env.NEXT_PHASE === "phase-production-build") {
+  env = process.env as any;
+} else {
+  env = cleanEnv(process.env, config);
+}
