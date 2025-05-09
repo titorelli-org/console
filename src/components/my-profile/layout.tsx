c15e34013fe2d86ffc87d1eeedb683615b5d2b36
ychebotaev <yury.79120345101@gmail.com>
2025-05-09 20:01:28 +0500

    fix: profile width same as header

--- src/components/my-profile/layout.tsx
+++ src/components/my-profile/layout.tsx
@@ -1,12 +1,11 @@
 import { AppHeader } from "@/components/site/header";
+import { Container } from "../site/container";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-background">
       <AppHeader />
-      <main className="container mt-16 max-w-[1080px] px-5 md:px-0 mx-auto py-8">
-        {children}
-      </main>
+      <Container className="mt-20">{children}</Container>
     </div>
   );
 }
