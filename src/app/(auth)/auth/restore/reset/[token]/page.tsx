69547ed68fb62f29e1a7a5d44af4a90a6be0f2bf
ychebotaev <yury.79120345101@gmail.com>
2025-06-20 21:08:16 +0500

    fix: build

--- src/app/(auth)/auth/restore/reset/[token]/page.tsx
+++ src/app/(auth)/auth/restore/reset/[token]/page.tsx
@@ -2,19 +2,19 @@ import { redirect } from "next/navigation";
 import { AuthorizationLayout } from "@/layouts/auth-layout";
 import { resetPassword } from "@/server-actions/auth/reset-password";
 import { ResetPasswordForm } from "@/components/auth/reset-password-form";
-import { getEmailService } from "@/lib/server/services/instances";
+import { getTokenService } from "@/lib/server/services/instances";
 
 export default async function ResetPasswordEmailLandingPage(props: {
   params: Promise<{
     token: string;
   }>;
 }) {
-  const emailService = getEmailService()
+  const tokenService = getTokenService();
 
   const token = (await props.params).token;
 
   const success =
-    await emailService.validateRestorePasswordTokenFromEmail(token);
+    await tokenService.validateRestorePasswordTokenFromEmail(token);
 
   if (!success) redirect("/auth/restore/reset/fail");
 
